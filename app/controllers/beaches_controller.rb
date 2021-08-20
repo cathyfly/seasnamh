e269dcbc371e6b3351f22e8d2b34c3b7241b0aa2
“[firstname <cathyflynnmedia@gmail.com>
2021-08-20 14:37:34 +0100

    stopped apis in beach con and index.html

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,7 +1,8 @@
 class BeachesController < ApplicationController
   before_action :set_beach, only: %i[ show edit update destroy ]
+
   #before_action :authenticate_user!
-  #before_action :ensure_admin,only:[:destroy,:create]
+  before_action :ensure_admin,only:[:destroy,:create]
 
   # GET /beaches or /beaches.json
   def index
@@ -12,22 +13,22 @@ class BeachesController < ApplicationController
     require 'uri'
     require 'openssl'
 
-    @url= 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=53.2895&lon=-6.1137'
-    @uri= URI(@url)
-    @response = Net::HTTP.get((@uri),{'User-Agent' => 'https://github.com/cathyfly/sea'})
-    @output = JSON.parse(@response)
+    # @url= 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=53.2895&lon=-6.1137'
+    #@uri= URI(@url)
+    #@response = Net::HTTP.get((@uri),{'User-Agent' => 'https://github.com/cathyfly/sea'})
+    #@output = JSON.parse(@response)
 
 
 
 
-    @url_tides = 'https://tides.p.rapidapi.com/tides?latitude=53.2895&longitude=-6.1377&duration=1440&interval=60'
-    @uri_tides= URI(@url_tides)
+    #@url_tides = 'https://tides.p.rapidapi.com/tides?latitude=53.2895&longitude=-6.1377&duration=1440&interval=60'
+    #@uri_tides= URI(@url_tides)
     #http = Net::HTTP.new(@uri_tides.host, @uri_tides.port)
     #http.use_ssl = true
     #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     #request = Net::HTTP::Get.new(@uri_tides)
-    @response_tides = Net::HTTP.get((URI.parse(@url_tides)),{'x-rapidapi-key' => 'b9b9f82b5bmshea905148572a99ap1c64c2jsn4c6d561ef553','x-rapidapi-host'=>'tides.p.rapidapi.com'})
+    #@response_tides = Net::HTTP.get((URI.parse(@url_tides)),{'x-rapidapi-key' => 'b9b9f82b5bmshea905148572a99ap1c64c2jsn4c6d561ef553','x-rapidapi-host'=>'tides.p.rapidapi.com'})
 
     #request["x-rapidapi-key"] = 'b9b9f82b5bmshea905148572a99ap1c64c2jsn4c6d561ef553'
     #request["x-rapidapi-host"] = 'tides.p.rapidapi.com'
@@ -35,7 +36,7 @@ class BeachesController < ApplicationController
     #response = http.request(request)
     #puts response.read_body
 
-    @output_tides = JSON.parse(@response_tides)
+    #@output_tides = JSON.parse(@response_tides)
 
 
   end
