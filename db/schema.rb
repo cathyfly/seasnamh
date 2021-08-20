44bda586db79650936b209dc89c5155a9036ced4
“[firstname <cathyflynnmedia@gmail.com>
2021-08-20 14:30:10 +0100

    nav sign/in out, admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_111255) do
+ActiveRecord::Schema.define(version: 2021_08_20_131918) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer "rater_id"
